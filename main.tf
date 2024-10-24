3ba69728761fce672149c6900aadb2dbb5c25332
news4me@free.fr <Magritte1234$>
2024-10-24 16:32:14 +0200

    Fix AWS Keys

--- main.tf
+++ main.tf
@@ -10,14 +10,14 @@ terraform {
 # Fournisseur AWS avec les identifiants directement dans le code
 provider "aws" {
   region     = "eu-west-1"
-  access_key = "AKIAQWTVESICI4XZJR7C"                     # Remplace par votre clé d'accès
-  secret_key = "fkcZI5joITR6zzmvK/GEMur+ZgGqeCG/NBFWafGm" # Remplace par votre clé secrète
+  access_key = "AKIAQWTVESICMJLB2CPB"                     # Remplace par votre clé d'accès
+  secret_key = "PBFCcBBakdKq39lHe8yGOzzbk11w15wlM7uZ04SS" # Remplace par votre clé secrète
 }
 
 
 # Créer un bucket S3 simple sans utiliser `acl`
 resource "aws_s3_bucket" "my_bucket" {
-  bucket = "philippe9229" # Remplace par un nom unique (your_name-5-digits)
+  bucket = "philippe9230" # Remplace par un nom unique (your_name-5-digits)
   tags = {
     Name        = "MySimpleBucket"
     Environment = "dev"
