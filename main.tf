356f8e9d654c279b55fa94cf0fd7bc166aa02595
news4me@free.fr <Magritte1234$>
2024-10-24 11:50:44 +0200

    Add GitIgnore and Update AWS Keys

--- main.tf
+++ main.tf
@@ -10,13 +10,13 @@ terraform {
 # Fournisseur AWS avec les identifiants directement dans le code
 provider "aws" {
   region     = "eu-west-1"
-  access_key = "AKIAQWTVESICABTFCGF7"                     # Remplace par votre clé d'accès
-  secret_key = "iHTbF8qA6mjGYQdGrEtYgr+yYlYJHu3VdttCE52W" # Remplace par votre clé secrète
+  access_key = "AKIAQWTVESICHEQ6FP4G"                     # Remplace par votre clé d'accès
+  secret_key = "Q2Fgwar8G8g+npX27z1VS/lAKwxL6erH6uVBech8" # Remplace par votre clé secrète
 }
 
 # Créer un bucket S3 simple sans utiliser `acl`
 resource "aws_s3_bucket" "my_bucket" {
-  bucket = "philippe922801" # Remplace par un nom unique (your_name-5-digits)
+  bucket = "philippe922802" # Remplace par un nom unique (your_name-5-digits)
   tags = {
     Name        = "MySimpleBucket"
     Environment = "dev"
