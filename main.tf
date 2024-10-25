4a5b098b61f6c35f49944d08765df89701ec8586
news4me@free.fr <Magritte1234$>
2024-10-25 13:51:13 +0200

    Use AWS Module

--- main.tf
+++ main.tf
@@ -7,19 +7,25 @@ terraform {
   } 
 }
 
+# Déclarer la variable "bucket_name"
+variable "bucket_name" {
+  type        = string
+  description = "philippe9239"
+}
+
 # Fournisseur AWS avec les identifiants directement dans le code
 provider "aws" {
   region     = "eu-west-1"
-  access_key = "AKIAQWTVESICHEQ6FP4G"                     # Remplace par votre clé d'accès
-  secret_key = "Q2Fgwar8G8g+npX27z1VS/lAKwxL6erH6uVBech8" # Remplace par votre clé secrète
 }
 
-
-# Créer un bucket S3 simple sans utiliser `acl`
-resource "aws_s3_bucket" "my_bucket" {
-  bucket = "philippe9234" # Remplace par un nom unique (your_name-5-digits)
+# Appel du module S3 Bucket depuis Terraform Registry
+module "s3-bucket" {
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "4.2.1"
+  # Utilisation de la variable définie dans TFC
+  bucket = var.bucket_name
   tags = {
     Name        = "MySimpleBucket"
     Environment = "dev"
   }
-}	
+}
